725eddca16656fcfbf76cc0ab12db2a74096199d
mareanovazquez <mareanovazquez@protonmail.com>
2023-12-19 16:57:55 -0300

    inicio rediseño estudio REC

---
+++ js/scrollAnimateTitleQH.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Función para verificar si un elemento está en la vista
+    let titleQueHacemos = document.getElementById('titleQueHacemos')
+
+    window.addEventListener('scroll', function(){
+        let queHacemosOffSetTop = document.getElementById('principal').offsetTop;
+        if (window.scrollY>=queHacemosOffSetTop) {
+            titleQueHacemos.classList.add('animate__fadeInDown');
+        }
+    })
+    
+});
+
+
