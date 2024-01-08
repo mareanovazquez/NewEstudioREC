456d0b3d0d854ea8b49aaffe673076d7cedc9c4b
Mariano Vazquez <78172034+mareanovazquez@users.noreply.github.com>
2024-01-07 21:34:46 -0300

    animación en main. falta resolver timing

--- js/typewriterMain.js
+++ js/typewriterMain.js
@@ -16,11 +16,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const line1MainElement = document.getElementById("line1Main");
     const line2MainElement = document.getElementById("line2Main");
 
+    // Define el texto que deseas agregar
+    const line1Text = "Creemos en la palabra escrita.";
+    const line2Text = "En su fuerza, en su poder de atracción, en su valor.";
+
+    // Agrega el texto a las etiquetas <p>
+    line1MainElement.textContent = "";
+    line2MainElement.textContent = "";
+
     // Define la velocidad de la animación
     const typingSpeed = 100; // Ajusta la velocidad según tu preferencia
 
     // Aplica la animación a cada elemento
-    typeWriter(line1MainElement, line1MainElement.textContent, typingSpeed);
-    typeWriter(line2MainElement, line2MainElement.textContent, typingSpeed);
+    typeWriter(line1MainElement, line1Text, typingSpeed);
+    typeWriter(line2MainElement, line2Text, typingSpeed);
 });
 
