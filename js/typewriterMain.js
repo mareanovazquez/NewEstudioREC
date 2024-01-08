120df1075e2177d1cda5bcd92b23fe660168c856
Mariano Vazquez <78172034+mareanovazquez@users.noreply.github.com>
2024-01-07 23:21:13 -0300

    animación en main

--- js/typewriterMain.js
+++ js/typewriterMain.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Función para animar un elemento tipo máquina de escribir
-    function typeWriter(element, text, speed) {
+    function typeWriter(element, text, speed, callback) {
         let i = 0;
         const intervalId = setInterval(function () {
             if (i < text.length) {
@@ -8,13 +8,18 @@ document.addEventListener("DOMContentLoaded", function () {
                 i++;
             } else {
                 clearInterval(intervalId);
+                if (callback) {
+                    callback(); // Llama al callback después de completar la animación
+                }
             }
         }, speed);
     }
 
-    // Selecciona los elementos con los IDs "line1Main" y "line2Main"
+    // Selecciona los elementos con los IDs "line1Main", "line2Main" y "btnPrincipal"
     const line1MainElement = document.getElementById("line1Main");
     const line2MainElement = document.getElementById("line2Main");
+    const btnPrincipalElement = document.getElementById("btnPrincipal");
+    const logotipoH1 = document.getElementById("logotipoH1")
 
     // Define el texto que deseas agregar
     const line1Text = "Creemos en la palabra escrita.";
@@ -25,10 +30,17 @@ document.addEventListener("DOMContentLoaded", function () {
     line2MainElement.textContent = "";
 
     // Define la velocidad de la animación
-    const typingSpeed = 100; // Ajusta la velocidad según tu preferencia
+    const typingSpeed = 65; // Ajusta la velocidad según tu preferencia
 
     // Aplica la animación a cada elemento
-    typeWriter(line1MainElement, line1Text, typingSpeed);
-    typeWriter(line2MainElement, line2Text, typingSpeed);
+    typeWriter(line1MainElement, line1Text, typingSpeed, function () {
+        // Este callback se ejecuta después de completar la animación de line1Main
+        typeWriter(line2MainElement, line2Text, typingSpeed, function () {
+            // Este callback se ejecuta después de completar la animación de line2Main
+            // Cambia el estilo del botón para mostrarlo después de la animación
+            logotipoH1.classList.add('visible')
+            btnPrincipalElement.style.display = 'block';
+            btnPrincipalElement.style.marginTop='30px'
+        });
+    });
 });
-
